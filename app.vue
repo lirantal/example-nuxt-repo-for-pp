19a479e8b658233520cda999f6ac93f786ce51d7
CodeSandbox <support@codesandbox.io>
2024-03-12 15:34:23 +0000

    Sandbox snapshot commit

--- app.vue
+++ app.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    
+    <NuxtWelcome />
   </div>
-  <NuxtPage />
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: {
-    enabled: true,
-
-    timeline: {
-      enabled: true,
-    },
-  },
+  devtools: { enabled: true },
 });
